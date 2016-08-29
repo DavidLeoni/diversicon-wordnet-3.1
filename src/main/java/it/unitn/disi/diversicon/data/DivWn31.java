24c0350abc0c886ebe41f54b328d1fed4b2e9887
David Leoni <davidleonigit-github@yahoo.com>
2016-08-29 19:50:41 +0200

    simplified directory structure

--- src/main/java/it/unitn/disi/diversicon/data/DivWn31.java
+++ src/main/java/it/unitn/disi/diversicon/data/DivWn31.java
@@ -21,22 +21,27 @@ public class DivWn31 extends DiversiconResource {
     /**
      * @since 0.1.0
      */
-    private static final String prefix = "classpath:/it/unitn/disi/diversicon/data/" + ID;
+    private static final String PREFIX = "wn31";    
     
     /**
      * @since 0.1.0
      */
-    public static final String SQL_URI = prefix + ".sql.xz";
+    private static final String CLASSPATH = "classpath:/"+ID;
     
     /**
      * @since 0.1.0
      */
-    public static final String H2DB_URI = prefix + ".h2.db.xz";
+    public static final String SQL_URI = CLASSPATH + ".sql.xz";
     
     /**
      * @since 0.1.0
      */
-    public static final String XML_URI = prefix + ".xml.xz";    
+    public static final String H2DB_URI = CLASSPATH + ".h2.db.xz";
+    
+    /**
+     * @since 0.1.0
+     */
+    public static final String XML_URI = CLASSPATH + ".xml.xz";    
     
     /**
      * @since 0.1.0
@@ -48,22 +53,20 @@ public class DivWn31 extends DiversiconResource {
      * @since 0.1.0
      */
     private static final DivWn31 INSTANCE = new DivWn31();
-   
 
     
     static {
-        INSTANCE.setId(ID);        
-        INSTANCE.setH2DbUri(prefix + ".h2.db.xz");
-        INSTANCE.setSqlUri(prefix + ".sql.xz");
-        INSTANCE.setXmlUri(prefix + ".xml.xz");
+        INSTANCE.setId(ID);  
+        INSTANCE.setPrefix(PREFIX);
+        INSTANCE.setH2DbUri(CLASSPATH + ".h2.db.xz");
+        INSTANCE.setSqlUri(CLASSPATH + ".sql.xz");
+        INSTANCE.setXmlUri(CLASSPATH + ".xml.xz");
         if (BuildInfo.hasProperties(DivWn31.class)){
-            BuildInfo buildInfo = BuildInfo.ofProperties(DivWn31.class);
+            BuildInfo buildInfo = BuildInfo.of(DivWn31.class);
             INSTANCE.setVersion(buildInfo.getVersion());
         } else {            
             throw new IllegalStateException("Couldn't find properties file " + BuildInfo.BUILD_PROPERTIES_PATH + " for class " + DivWn31.class.getCanonicalName());
-        }
-        
-        
+        }               
     }
        
     /**
